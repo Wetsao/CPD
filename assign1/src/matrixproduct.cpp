3b253caf146a1f3e7583c40ccb2889c2794bc2e2
Alexandre Morais <up201906049@fc.up.pt>
2025-03-06 11:53:16 +0000

    multline added and multblock fixed

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -75,14 +75,7 @@ void OnMult(int m_ar, int m_br)
 
 void OnMultLine(int m_ar, int m_br)
 {
-    
-    
-}
-
-
-void OnMultBlock(int m_ar, int m_br, int bkSize)
-{
-	SYSTEMTIME Time1, Time2;
+    SYSTEMTIME Time1, Time2;
 	
 	char st[100];
 	double temp;
@@ -110,18 +103,79 @@ void OnMultBlock(int m_ar, int m_br, int bkSize)
 
     Time1 = clock();
 
+	for(i=0; i<m_ar; i++)
+    {    for( k=0; k<m_ar; k++ )
+        { for( j=0; j<m_br; j++)
+            {    
+                phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+            }
+        }
+    }
+
+
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+}
+
+
+void OnMultBlock(int m_ar, int m_br, int bkSize)
+{
+	SYSTEMTIME Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k, sub_chunk, idx;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phc[i*m_br + j] = (double)0;
+
+
+
+    Time1 = clock();
+
 	for(i=0; i<m_ar; i++) {
 		for(j=0; j<m_ar; j+=bkSize) {
 			for(k=0; k<m_ar; k+=bkSize){
-				for(int sub_chunk = 0; sub_chunk< bkSize; sub_chunk++) {
-					for(int idx = 0; idx<bkSize; idx++) {
-						phc[i * bkSize + j + idx] += pha[i * bkSize + k + sub_chunk] * phb[k * bkSize + sub_chunk * bkSize + j + idx];
+				for(sub_chunk = 0; sub_chunk< bkSize; sub_chunk++) {
+					for(idx = 0; idx<bkSize; idx++) {
+						phc[i * m_ar + j + idx] += pha[i * m_ar + k + sub_chunk] * phb[k * m_ar + sub_chunk * m_ar + j + idx];
 					}
 				}
 			}
 		}
 	}
-
 
     Time2 = clock();
 	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
